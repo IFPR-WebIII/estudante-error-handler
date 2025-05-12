ac905f72cd16359c8da71e5e758c875b05084da0
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2025-05-12 20:14:59 -0300

    Update testes unitários

---
+++ src/test/java/br/edu/ifpr/estudante_error_handler/controllers/EstudanteIntegrationTest.java
@@ -0,0 +1,12 @@
+package br.edu.ifpr.estudante_error_handler.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EstudanteIntegrationTest {
+
+    
+    
+}
--- src/test/java/br/edu/ifpr/estudante_error_handler/services/EstudanteServiceUnitTest.java
+++ src/test/java/br/edu/ifpr/estudante_error_handler/services/EstudanteServiceUnitTest.java
@@ -1,14 +1,15 @@
 package br.edu.ifpr.estudante_error_handler.services;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Spy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -39,14 +40,64 @@ public class EstudanteServiceUnitTest {
     @Test
     public void deveLancarUmaExcecaoAoCriarEstudantesComMesmaMatricula(){
 
+        //Arrange
+        Estudante estudante = new Estudante("Joana", LocalDate.of(1989, 04, 26), "12181117");
+
         //Act
         estudanteService.cadastrarEstudante(estudante);
 
+        //Assert
         assertThrows(EstudanteException.class, () -> {
             estudanteService.cadastrarEstudante(estudante);
         });
+    }
+
+    @Test
+    public void deveRetornarUmEstudanteParaUmaMatriculaExistente(){
+
+        //Arrange
+        Estudante estudante = new Estudante("Joana", LocalDate.of(1989, 04, 26), "12181119");
+
+        //Act
+        estudanteService.cadastrarEstudante(estudante);
+
+        Estudante encontrado = estudanteService.buscarPorMatricula("12181119");
+
+        //Assert
+        assertNotNull(encontrado);
+        assertEquals("12181119", encontrado.getMatricula());
+    }
+
+    @Test
+    public void deveLancarUmaExcecaoAoBuscarPorMatriculaInexistente(){
+
+        EstudanteException exception = assertThrows(EstudanteException.class, ()->{
+            estudanteService.buscarPorMatricula("9999999");
+        });
+
+        assertEquals("estudante não encontrado", exception.getMessage());
 
     }
+
+    @Test
+    public void deveRetornarUmaListaDosEstudantesCadastrados(){
+        //Arrange
+        Estudante estudante1 = new Estudante("Jean", LocalDate.of(1989, 04, 26), "11122119");
+        Estudante estudante2 = new Estudante("Claude", LocalDate.of(1989, 04, 26), "908230813");
+
+        //Act
+        estudanteService.cadastrarEstudante(estudante1);
+        estudanteService.cadastrarEstudante(estudante2);
+        
+        List<Estudante> estudantes = estudanteService.buscarTodos();
+
+        //Asserts
+        assertTrue(estudantes.contains(estudante1));
+        assertTrue(estudantes.contains(estudante2));
+        assertThat(estudantes.size()).isGreaterThan(1);
+
+    }
+
 
 
     
