2b94e25bd6e5cc363f2f0ca9fe17f84de90b4a68
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2025-05-12 20:21:43 -0300

    Update Integration Test

--- src/test/java/br/edu/ifpr/estudante_error_handler/controllers/EstudanteIntegrationTest.java
+++ src/test/java/br/edu/ifpr/estudante_error_handler/controllers/EstudanteIntegrationTest.java
@@ -1,12 +1,25 @@
 package br.edu.ifpr.estudante_error_handler.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class EstudanteIntegrationTest {
 
-    
+    @Autowired
+    private MockMvc mockRequest;
+
+    public void deRetornarAViewDeCadastroDoEstudate(){
+
+        mockRequest.perform(get("/estudantes"))
+        
+
+    }
     
 }
